e6e55cfc5b345c4aaf57f79293e37a11e073df76
esatozholcek <esatozholcek@gmail.com>
2023-01-05 11:18:13 +0300

    05.01.2023

--- src/main/java/com/example/snakegame/Main.java
+++ src/main/java/com/example/snakegame/Main.java
@@ -11,11 +11,8 @@ public class Main extends Application {
 
 	
     public void start(Stage primaryStage) throws Exception {
-        //Font.loadFont(Font.loadFont(Object.class.getResource("C:\\Users\\ÖZHÖLÇEK\\IdeaProjects\\SnakeGame\\src\\main\\resources\\com\\example\\snakegame\\Eyvindur.ttf").toExternalForm(), 10));
         Pane root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         Scene scene = new Scene(root);
-        //scene.getStylesheets().add(getClass().getResource("menu.css").toExternalForm());
-        //scene.getStylesheets().add(String.valueOf(getClass().getResource("C:\\Users\\ÖZHÖLÇEK\\IdeaProjects\\SnakeGame\\src\\main\\resources\\com\\example\\snakegame\\Eyvindur.ttf")));
         scene.getStylesheets().add("menu.css");
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(scene);
